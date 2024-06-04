af7348ba1cfaf6e1dde98240d44b64a531379cb0
Junghwa8870 <suejunghwa@gmail.com>
2024-06-04 17:31:01 +0900

    리액트 네이티브

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, View } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // 모달의 렌더링 상태를 나타내는 변수
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [todoGoals, setTodoGoals] = useState([]);
+
+  // 추가 버튼을 누르면 할 일 목록을 추가하는 함수
+  const addGoalHandler = (enteredGoalText) => {
+    // useState setter 메서드의 스냅샷 방식
+    // 콜백 함수의 매개값은 해당 상태 변수의 최신 값이 전달됨.
+    setTodoGoals((currentTodoGoals) => [
+      ...currentTodoGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+
+    endAddGoalHandler();
+  };
+
+  const deleteGoalHandler = (id) => {
+    setTodoGoals((currentTodoGoals) => {
+      return currentTodoGoals.filter((goal) => goal.id !== id);
+    });
+  };
+
+  // 할 일 추가 모달을 띄워주는 함수
+  const startAddGoalhandler = () => {
+    setModalIsVisible(true);
+  };
+
+  const endAddGoalHandler = () => {
+    setModalIsVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light' />
+      <View style={styles.appContainer}>
+        <Button
+          title='할 일 추가하려면 누르세요!'
+          color='#5e0acc'
+          onPress={startAddGoalhandler}
+        />
+        {/* modalIsVisible && 를 직접 구현할 필요 없음. */}
+        <GoalInput
+          visible={modalIsVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+        <View style={styles.goalsContainer}>
+          {/* 
+          ScrollView는 전체 화면이 렌더링 될 때 안의 항목들을 전부 렌더링합니다.
+          이로 인해 성능 상의 저하가 나타날 수 있습니다.
+          (보이지 않는 영역까지 렌더링을 진행하기 때문에 목록이 많다면 로딩이 길어짐.)
+          FlatList는 보이는 영역만 일단 렌더링을 진행하고, 나머지 항목들은
+          스크롤 움직임이 발생하면 그 때 그 때 렌더링을 진행합니다.
+        */}
+          <FlatList
+            data={todoGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                />
+              );
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+          ></FlatList>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
---
+++ components/GoalInput.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Image,
+  Modal,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
+
+const GoalInput = ({ visible, onAddGoal, onCancel }) => {
+  // 입력창의 상태를 관리하는 변수를 React에서 사용하는 useState 훅을 활용하여 선언.
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  // 사용자가 내용을 입력할 때 해당 입력값을 가져오는 함수
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    onAddGoal(enteredGoalText);
+    setEnteredGoalText('');
+  };
+
+  return (
+    // 조건부 렌더링을 직접 구현하지 않고 Modal의 visible props를 활용해서 모달을 열고 닫을 수 있음.
+    <Modal visible={visible} animationType='slide'>
+      <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require('../assets/chunbae.jpg')} />
+        <TextInput
+          style={styles.textInput}
+          placeholder='할 일을 입력하세요!'
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              title='할 일 추가하기'
+              onPress={addGoalHandler}
+              color='#b180f0'
+            />
+          </View>
+          <View style={styles.button}>
+            <Button title='취소' color='#f31282' onPress={onCancel} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#311b6b',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    margin: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#e4d0ff',
+    backgroundColor: '#e4d0ff',
+    color: '#120438',
+    borderRadius: 6,
+    width: '100%',
+    padding: 16,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  button: {
+    width: '30%',
+    marginHorizontal: 8,
+  },
+});
